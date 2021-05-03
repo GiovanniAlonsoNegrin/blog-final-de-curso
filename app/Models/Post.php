22b82e62ae48b3ba47be96efa6d68476c1c738da
Giovanni Alonso <74564389+GiovanniAlonsoNegrin@users.noreply.github.com>
2021-05-03 11:36:05 +0100

    Empezando proyecto

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    //Relatios
+
+    // One to many reverse
+    public function user(){
+        return $this->blongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->blongsTo(Category::class);
+    }
+    // Many to many
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+    // One tu one polimorphic
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use App\Models\Category;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Storage;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Storage::deleteDirectory('posts'); // This method delete the posts directory if exists.
+        Storage::makeDirectory('posts'); //This method create a new directory with name posts.
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
+    }
+}
