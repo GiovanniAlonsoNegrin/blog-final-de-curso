78f349ebc16227ff2beb82205a9b128b1cfc6913
Giovanni Alonso <74564389+GiovanniAlonsoNegrin@users.noreply.github.com>
2021-05-27 18:19:18 +0100

    validate all comments

--- resources/views/livewire/admin/validate-all-comments.blade.php
+++ resources/views/livewire/admin/validate-all-comments.blade.php
@@ -1,8 +1,20 @@
 <div>
-    <button wire:click="save" class="btn btn-success btn-sm float-right">Validar todo</button>
+    <button wire:click="save" id="button" class="btn btn-success btn-sm float-right">Validar todo</button>  
 
-    @foreach ($post->comments as $comment)
-        {{ $comment->status }}
-    @endforeach
-    
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        window.onload = function () {
+            $('#button').click(function () { 
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Genial!',
+                    text: 'Los comentarios se validaron con éxito',
+                    confirmButtonText: 'ok'
+                }).then( function(){
+                    window.location.reload();
+                });
+            }); 
+        };
+    </script> 
 </div>
